e2d6ead2bd9a0daaee08fbab140362fc54883e73
KibalinMax <kibalaspb@mail.ru>
2023-05-26 09:28:32 +0300

    Add funcs change

--- funcs.py
+++ funcs.py
@@ -1,3 +1,15 @@
 def title_name():
+    """
+    Функция делает все буквы в строке заглавными
+    :return: None
+    """
     user_input = input()
     print(user_input.lower())
+
+def swapcase_name():
+    """
+    Функция делает первые буквы слов в строке заглавными
+    :return: None
+    """
+    user_input_1 = input()
+    print(user_input_1.swapcase())
